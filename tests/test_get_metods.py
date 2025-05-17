68805bcd3a67b72cdc16c69d8e72052272d2dd39
Togarini0 <malary0@gmail.com>
2025-05-17 18:05:49 +0300

    4

--- tests/test_get_metods.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from data.get_data import GET_ENDPOINTS  # Данные для GET
-
-@pytest.mark.parametrize("endpoint, params, expected_status", GET_ENDPOINTS)
-def test_get_methods(api_client, endpoint, params, expected_status):
-    response = api_client.get(endpoint, params=params)
-    assert response.status_code == expected_status
-    assert "data" in response.json()  # Пример проверки структуры ответа
